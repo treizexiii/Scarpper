05853ba9cb66c3df7a9821900fb499c5b71857c3
Treize <Jonathan.rougier@hotmail.fr>
2021-02-18 15:13:30 +0100

    version 1.0

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using HtmlAgilityPack;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Scrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> urls = new List<string>();
+            urls.Add("https://www.clubic.com/");
+
+            Script.MapingWebAsync(urls, 0);
+            //List<string> newResults = new List<string>();
+                
+            //results.Select(r => Script.MapingWeb(r));
+
+            //while (true)
+            //{
+            //    newResults.Clear();
+
+            //    foreach (var result in results)
+            //    {
+            //        Console.WriteLine(result);
+            //        newResults = Script.MapingWeb(result);
+
+            //        //foreach (var newelement in newTest)
+            //        //{
+            //        //    Console.WriteLine(newelement);
+            //        //    results.Add(newelement);
+            //        //}
+            //    }
+
+            //    results = newResults;
+            //}
+
+
+        }
+    }
+}
---
+++ Script.cs
@@ -0,0 +1,64 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace Scrapper
+{
+    public class Script
+    {
+        public static void MapingWebAsync(List<string> urls, int index)
+        {
+            Console.WriteLine(index);
+            WebClient client = new WebClient();
+            string datas;
+
+            try
+            {
+                datas = client.DownloadString(urls[index]);
+            }
+            catch (Exception e)
+            {
+                if (index <= 0)
+                {
+                    index = 1;
+                }
+                Console.WriteLine(urls[index] + ": " + e.Message);
+                datas = client.DownloadString(urls[index - 1]);
+            }
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(datas);
+
+            var nodes = doc.DocumentNode.SelectNodes("//a");
+
+            if (nodes != null)
+            {
+                foreach (var node in nodes)
+                {
+                    if (node.Attributes["href"] != null && node.Attributes["href"].Value.Length >= 5)
+                    {
+                        if (node.Attributes["href"].Value.Substring(0, 5) == "https")
+                        {
+                            string[] urlDestruct = node.Attributes["href"].Value.Split('/');
+                            var newUrl = "https://" + urlDestruct[2];
+
+                            if (!urls.Any(r => r == newUrl))
+                            {
+                                Console.WriteLine(newUrl);
+                                urls.Add(newUrl);
+                                MapingWebAsync(urls, urls.Count - 1);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(urls[index] + ": no link to follow.");
+                MapingWebAsync(urls, urls.Count - 2);
+            }
+        }
+    }
+}
